8de223fdeecde00d973105f12e7877f0a7896d99
Mara-Li <lili.simonetti@outlook.fr>
2024-02-12 15:44:22 +0000

    rename folder

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     baseUrl: "www.mara-li.fr",
-    contentDir: "img",
+    ogImageDir: "img",
     ignorePatterns: ["private", "templates", ".obsidian", "hidden"],
     defaultDateType: "modified",
     theme: {
