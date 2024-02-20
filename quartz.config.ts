a0976f724c36310c2e353b054be0158760cc467a
Mara <mara-li@outlook.fr>
2024-02-20 19:44:34 +0100

    fix: change image folder

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,92 +1,92 @@
-import { QuartzConfig } from "./quartz/cfg"
-import * as Plugin from "./quartz/plugins"
+import { QuartzConfig } from "./quartz/cfg";
+import * as Plugin from "./quartz/plugins";
 
 const config: QuartzConfig = {
-  configuration: {
-    pageTitle: "🦉 Owlly Lab",
-    enableSPA: true,
-    locale: "fr-FR",
-    enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    baseUrl: "www.mara-li.fr",
-    ogImageDir: "img",
-    ignorePatterns: ["private", "templates", ".obsidian", "hidden"],
-    defaultDateType: "modified",
-    theme: {
-      cdnCaching: true,
-      typography: {
-        header: "Karla",
-        body: "IBM Plex Sans",
-        code: "Ubuntu Mono",
-      },
-      colors: {
-        lightMode: {
-          light: "#f5f2e9",
-          lightgray: "#e0dcd3",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#384b73",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-        },
-        darkMode: {
-          light: "#1a1915",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#5d7da5",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-        },
-      },
-    },
-  },
-  plugins: {
-    transformers: [
-      Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        // you can add 'git' here for last modified from Git
-        // if you do rely on git for dates, ensure defaultDateType is 'modified'
-        priority: ["frontmatter", "git", "filesystem"],
-      }),
-      Plugin.Latex({ renderEngine: "katex" }),
-      Plugin.SyntaxHighlighting({
-        // uses themes bundled with Shikiji, see https://shikiji.netlify.app/themes
-        theme: {
-          light: "github-light",
-          dark: "github-dark",
-        },
-        // set this to 'true' to use the background color of the Shikiji theme
-        // if set to 'false', will use Quartz theme colors for background
-        keepBackground: false,
-      }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "relative" }),
-      Plugin.Description(),
-      Plugin.HardLineBreaks(),
-    ],
-    filters: [Plugin.RemoveDrafts()],
-    emitters: [
-      Plugin.AliasRedirects(),
-      Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
-      Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
-      Plugin.ContentIndex({
-        enableSiteMap: true,
-        enableRSS: true,
-      }),
-      Plugin.Assets(),
-      Plugin.Static(),
-      Plugin.NotFoundPage(),
-    ],
-  },
-}
+	configuration: {
+		pageTitle: "🦉 Owlly Lab",
+		enableSPA: true,
+		locale: "fr-FR",
+		enablePopovers: true,
+		analytics: {
+			provider: "plausible",
+		},
+		baseUrl: "www.mara-li.fr",
+		ogImageDir: "_assets/img",
+		ignorePatterns: ["private", "templates", ".obsidian", "hidden"],
+		defaultDateType: "modified",
+		theme: {
+			cdnCaching: true,
+			typography: {
+				header: "Karla",
+				body: "IBM Plex Sans",
+				code: "Ubuntu Mono",
+			},
+			colors: {
+				lightMode: {
+					light: "#f5f2e9",
+					lightgray: "#e0dcd3",
+					gray: "#b8b8b8",
+					darkgray: "#4e4e4e",
+					dark: "#2b2b2b",
+					secondary: "#384b73",
+					tertiary: "#84a59d",
+					highlight: "rgba(143, 159, 169, 0.15)",
+				},
+				darkMode: {
+					light: "#1a1915",
+					lightgray: "#393639",
+					gray: "#646464",
+					darkgray: "#d4d4d4",
+					dark: "#ebebec",
+					secondary: "#5d7da5",
+					tertiary: "#84a59d",
+					highlight: "rgba(143, 159, 169, 0.15)",
+				},
+			},
+		},
+	},
+	plugins: {
+		transformers: [
+			Plugin.FrontMatter(),
+			Plugin.CreatedModifiedDate({
+				// you can add 'git' here for last modified from Git
+				// if you do rely on git for dates, ensure defaultDateType is 'modified'
+				priority: ["frontmatter", "git", "filesystem"],
+			}),
+			Plugin.Latex({ renderEngine: "katex" }),
+			Plugin.SyntaxHighlighting({
+				// uses themes bundled with Shikiji, see https://shikiji.netlify.app/themes
+				theme: {
+					light: "github-light",
+					dark: "github-dark",
+				},
+				// set this to 'true' to use the background color of the Shikiji theme
+				// if set to 'false', will use Quartz theme colors for background
+				keepBackground: false,
+			}),
+			Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+			Plugin.GitHubFlavoredMarkdown(),
+			Plugin.TableOfContents(),
+			Plugin.CrawlLinks({ markdownLinkResolution: "relative" }),
+			Plugin.Description(),
+			Plugin.HardLineBreaks(),
+		],
+		filters: [Plugin.RemoveDrafts()],
+		emitters: [
+			Plugin.AliasRedirects(),
+			Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
+			Plugin.ContentPage(),
+			Plugin.FolderPage(),
+			Plugin.TagPage(),
+			Plugin.ContentIndex({
+				enableSiteMap: true,
+				enableRSS: true,
+			}),
+			Plugin.Assets(),
+			Plugin.Static(),
+			Plugin.NotFoundPage(),
+		],
+	},
+};
 
-export default config
+export default config;
