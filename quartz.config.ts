22b1d913c04976903c5b866a7b1980c1fbbce975
Mara <mara-li@outlook.fr>
2024-04-18 21:48:21 +0200

    yoink

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
 			cdnCaching: true,
 			fontOrigin: "googleFonts",
 			typography: {
-				header: "Overpass",
+				header: "Sora",
 				body: "Karla",
 				code: "Ubuntu Mono",
 			},
